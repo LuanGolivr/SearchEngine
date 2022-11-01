d296d7e44372b1ebdf61919aa2f1726ea466918f
Luan Oliveira <luangoncalvesoliveira@gmail.com>
2022-11-01 15:55:31 -0300

    A new way to use webcrawler

--- webCrawler.py
+++ webCrawler.py
@@ -1,3 +1,6 @@
+'''
+        THIS IS ONE WAY TO DO
+
 import requests
 import re
 
@@ -13,4 +16,20 @@ for link in search:
         with open("link.txt","a") as file:
             file.write(f'{link}\n')
     check.add(link)
+'''
 
+'''    LET'S TRY ANOTHER WAY '''
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as soup
+import re
+
+
+url = urlopen("https://g1.globo.com/?utm_source=globo.com&utm_medium=header/")
+bsobj = soup(url.read(), features="html.parser")
+
+caching = set()
+for link in bsobj.findAll('a'):
+    if 'href' in link.attrs and link.attrs['href'] not in caching:
+        print(link.attrs['href'])
+        caching.add(link.attrs['href'])
