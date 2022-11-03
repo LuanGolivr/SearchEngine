c721adda9cea4d201f9561e416e21652d0e1984c
Luan Oliveira <luangoncalvesoliveira@gmail.com>
2022-11-02 21:30:43 -0300

    Update

--- SearchEngine/SearchEngine/Controllers/HomeController.cs
+++ SearchEngine/SearchEngine/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace SearchEngine.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
