7d14d3ab782e8271dcb8434267da9adbe5295d1a
Luan Oliveira <luangoncalvesoliveira@gmail.com>
2022-11-08 18:38:13 -0300

    Small changes

--- SearchEngine/src/database/mongoConnection.ts
+++ SearchEngine/src/database/mongoConnection.ts
@@ -6,11 +6,9 @@ dotenv.config();
 
 export const mongoConect = async ()=>{
     try {
-        console.log('Connecting to mongoDB');
         await connect(process.env.MONGO_URL as string);
-        console.log('Connected sucessfuly');
     }
     catch(error) {
-        console.log('Error, we could not connect to MongoDB');
+        
     }
 }
