98fae526352590637f0ba15dbad1a3c1dad2616f
Luan Oliveira <86797008+LuanGolivr@users.noreply.github.com>
2022-11-14 19:48:19 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-htmlText = requests.get('https://g1.globo.com/').text
-soup = BeautifulSoup(htmlText, 'lxml')
-tag = soup.findAll('a')
-print(tag)
-
